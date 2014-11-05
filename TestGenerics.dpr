a8331e50469a81f0b2b7897884250e2fe0b65f7e
FiachraBonner <fiachrabonner@hotmail.com>
2014-11-05 16:00:14 +0000

    Added Code

---
+++ TestGenerics.dpr
@@ -0,0 +1,14 @@
+program TestGenerics;
+
+uses
+  Vcl.Forms,
+  MainF in 'MainF.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
